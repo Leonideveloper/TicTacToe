48a173fc9192d8b3c6bf686067afedd84ca628c8
Semenov Leonid <leonidandand@gmail.com>
2013-09-28 18:39:20 +0400

    see description
    getting rid of the unnecessary TicTacToeController class

--- TicTacToe/src/main/java/com/gmail/leonidandand/tictactoe/game/TicTacToeActivity.java
+++ TicTacToe/src/main/java/com/gmail/leonidandand/tictactoe/game/TicTacToeActivity.java
@@ -3,11 +3,16 @@ package com.gmail.leonidandand.tictactoe.game;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.gmail.leonidandand.tictactoe.game.model.TicTacToeModel;
+import com.gmail.leonidandand.tictactoe.game.model.TicTacToeModelImpl;
+import com.gmail.leonidandand.tictactoe.game.players.PlayerFactoryImpl;
 import com.gmail.leonidandand.tictactoe.game.players.PlayerTypes;
+import com.gmail.leonidandand.tictactoe.game.view.android_impl.TicTacToeViewAndroidImpl;
 
 public class TicTacToeActivity extends Activity {
 
-    private static TicTacToeController controller;
+    private static TicTacToeModel model;
+    private static TicTacToeViewAndroidImpl view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +22,9 @@ public class TicTacToeActivity extends Activity {
 
         boolean restartingOfActivity = (savedInstanceState != null);
         if (restartingOfActivity) {
-            controller.onRestoreState(this, savedInstanceState);
+            restoreState(savedInstanceState);
         } else {
-            controller = createController();
+            create();
         }
     }
 
@@ -29,17 +34,28 @@ public class TicTacToeActivity extends Activity {
         TicTacToeContext.setSecondPlayerType(PlayerTypes.AI.NORMAL);
     }
 
-    private TicTacToeController createController() {
-        return new TicTacToeController(
-                this,
-                TicTacToeContext.getGameBoardDimension(),
-                TicTacToeContext.getFirstPlayerType(),
-                TicTacToeContext.getSecondPlayerType()
+    private void create() {
+        model = new TicTacToeModelImpl(
+            TicTacToeContext.getGameBoardDimension(),
+            new PlayerFactoryImpl(),
+            TicTacToeContext.getFirstPlayerType(),
+            TicTacToeContext.getSecondPlayerType()
         );
+
+        view = new TicTacToeViewAndroidImpl(model, this);
+    }
+
+    private void restoreState(Bundle savedInstanceState) {
+        view = new TicTacToeViewAndroidImpl(model, this, view);
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
-        controller.onSaveState(this, outState);
+        super.onSaveInstanceState(outState);
+        saveState(outState);
+    }
+
+    private void saveState(Bundle outState) {
+        // Do nothing
     }
 }
