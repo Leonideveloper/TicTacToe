fc2f69946862933c9e5785a9ea63b9eedcca5ce1
Semenov Leonid <leonidandand@gmail.com>
2013-10-20 22:09:28 +0400

    mini-refactoring

--- TicTacToe/src/main/java/com/gmail/landanurm/tictactoe/game/TicTacToeController.java
+++ TicTacToe/src/main/java/com/gmail/landanurm/tictactoe/game/TicTacToeController.java
@@ -5,11 +5,10 @@ import android.os.Bundle;
 
 import com.gmail.landanurm.tictactoe.game.model_view.model.TicTacToeModel;
 import com.gmail.landanurm.tictactoe.game.model_view.model.TicTacToeModelImpl;
-import com.gmail.landanurm.tictactoe.game.model_view.model.player.PlayersFactory;
 import com.gmail.landanurm.tictactoe.game.model_view.view.TicTacToeView;
 import com.gmail.landanurm.tictactoe.game.model_view.view.TicTacToeViewImpl;
-import com.gmail.landanurm.tictactoe.game.players.PlayersFactoryImpl;
 import com.gmail.landanurm.tictactoe.game.players.PlayerTypes;
+import com.gmail.landanurm.tictactoe.game.players.PlayersFactoryImpl;
 import com.gmail.landanurm.tictactoe.game.view_components_provider_android_impl.ViewComponentsProviderAndroidImpl;
 
 import java.io.Serializable;
@@ -18,12 +17,11 @@ import java.io.Serializable;
  * Created by Leonid on 15.10.13.
  */
 public class TicTacToeController {
-    private final Activity activity;
-    private final Integer gameBoardDimension;
-    private final PlayersFactory playersFactory;
-    private final String firstPlayerType;
-    private final String secondPlayerType;
+    private static final Integer gameBoardDimension = 5;
+    private static final String firstPlayerType = PlayerTypes.HUMAN;
+    private static final String secondPlayerType = PlayerTypes.HUMAN;
 
+    private final Activity activity;
     private ViewComponentsProviderAndroidImpl viewComponentsProvider;
     private TicTacToeView view;
     private TicTacToeModel model;
@@ -31,10 +29,6 @@ public class TicTacToeController {
 
     public TicTacToeController(Activity activity) {
         this.activity = activity;
-        this.gameBoardDimension = 5;
-        this.playersFactory = new PlayersFactoryImpl();
-        this.firstPlayerType = PlayerTypes.HUMAN;
-        this.secondPlayerType = PlayerTypes.HUMAN;
     }
 
     public void startGame() {
@@ -46,7 +40,9 @@ public class TicTacToeController {
 
     private TicTacToeModel createModel() {
         return new TicTacToeModelImpl(gameBoardDimension,
-                playersFactory, firstPlayerType, secondPlayerType);
+                                      new PlayersFactoryImpl(),
+                                      firstPlayerType,
+                                      secondPlayerType);
     }
 
     private ViewComponentsProviderAndroidImpl createViewComponentsProvider() {
@@ -68,17 +64,17 @@ public class TicTacToeController {
     }
 
     public void restoreStateFrom(Bundle savedState) {
-        model = restoreModelFrom(savedState);
+        model = getModelRestoredFrom(savedState);
         viewComponentsProvider = createViewComponentsProviderFrom(savedState);
         view = createView();
     }
 
-    private TicTacToeModel restoreModelFrom(Bundle savedState) {
+    private TicTacToeModel getModelRestoredFrom(Bundle savedState) {
         return (TicTacToeModel) savedState.getSerializable(BundleKeys.model);
     }
 
     private ViewComponentsProviderAndroidImpl createViewComponentsProviderFrom(Bundle savedState) {
-        Serializable state = savedState.getSerializable(BundleKeys.viewComponentsState);
-        return new ViewComponentsProviderAndroidImpl(gameBoardDimension, activity, state);
+        return new ViewComponentsProviderAndroidImpl(gameBoardDimension, activity,
+                                    savedState.getSerializable(BundleKeys.viewComponentsState));
     }
 }
